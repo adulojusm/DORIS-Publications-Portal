1593e259c5d06aaf0b55ec57f77a0de45e1caf20
ziros94 <alvi.kabir@nyu.edu>
2014-06-10 12:10:41 -0400

    updated environment

---
+++ app/config.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
