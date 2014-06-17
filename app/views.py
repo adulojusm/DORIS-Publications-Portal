01e9001d798d04258ef0111363b5e0e3fd4b0645
ziros94 <alvi.kabir@nyu.edu>
2014-06-17 10:56:53 -0400

    minor changes

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import Flask, render_template, request, flash, url_for,redirect
+from flask import Flask, render_template, request, flash, url_for,redirect,make_response
 from forms import SearchForm
 from models import db,Agency,Category,Type,Document
 import datetime
@@ -10,9 +10,7 @@ app.secret_key = 'development key'
 @app.route('/index',methods=['GET', 'POST'])
 def index():
     form = SearchForm()
-    '''if form.user_input is None:
-        flash('Please enter a search entry.')
-    else:'''
+
     return render_template("index.html", form=form)
 
 
@@ -20,16 +18,23 @@ def index():
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
+    if request.method == 'POST':
 
         search = request.form['user_input']
+        if not search:
+            flash("Please enter a search")
+            return redirect(url_for('index'))
         agency = request.form['agency']
         category = request.form['category']
         types = request.form['type']
-
         return render_template("res.html", search=search, agency=agency, category=category, types=types, method='post')
 
+    else:
 
-@app.route('/publication')
+        return render_template("res.html")
+
+
+@app.route('/publication', methods=['GET'])
 def publication():
     return render_template("publication.html")
 
@@ -37,6 +42,7 @@ def publication():
 def about():
 	return render_template("about.html")
 
+'''
 @app.route('/testdb')
 def testdb():
     date1 = datetime.date(2011,07,21)
@@ -47,4 +53,6 @@ def testdb():
     newdoc.tid=1
     db.session.add(newdoc)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for('index')
+    )
+    '''
