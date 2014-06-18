f88e427d27dc56cf6f6a46895022e1f2718c696c
Palisand <palisand@nyu.edu>
2014-06-18 17:14:54 -0400

    Early query processing attempt

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/publications'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@10.132.41.31/publications'
 
 from models import db
 db.init_app(app)
--- app/views.py
+++ app/views.py
@@ -23,7 +23,10 @@ def results():
         agency = request.form['agency']
         category = request.form['category']
         types = request.form['type']
-        return render_template("res.html", search=search, agency=agency, category=category, types=types, method='post')
+        
+        results = process_query(search, agency, category, types)
+        
+        return render_template("res.html", results = results, method='post')
 
     else:
 
@@ -41,7 +44,6 @@ def about():
 
 @app.route('/testdb')
 def testdb():
-
     date1 = datetime.date(2011,07,21)
     category = Category('Education')
     db.session.add(category)
@@ -50,6 +52,31 @@ def testdb():
     newdoc.category = category
     db.session.add(newdoc)
     aquery = Document.query.join(Category).filter(Document.id == 1).first()
+    
+    aqueryBeginning = Document.query.join()
+    aqueryNextStep = aqueryBeginning.filter(Document == 1)
+    aqueryFinalStep = aqueryNextStep.first()
+    
     db.session.commit()
-    flash(str(aquery.id) + str(aquery.cid))
+    flash(str(aquery.title) + ' ' + str(aquery.cid))
     return redirect(url_for('index'))
+
+
+#GOES IN A MODULE (LATER) ...
+
+def process_query(search, _agency, _category, _types):
+
+	a_results = Agency.query.all()
+	c_results = Category.query.all()
+	t_results = Type.query.all()
+	
+	if _agency != "All Agencies":
+		a_results = Agency.query.filter(Agency.agency == _agency).first() #name
+	if _category != "All Categories":
+		c_results = Category.query.filter(Category.category == _category).first()
+	if _types != "All Types":
+		t_results = Type.query.filter(Type.type == _types).first()
+
+	
+	
+	return None
