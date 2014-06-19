dcd815b4aacd0c5b525d4028b5cc97f209660ad9
ziros94 <alvi.kabir@nyu.edu>
2014-06-19 16:44:25 -0400

    added description searchability

--- app/query_functions.py
+++ app/query_functions.py
@@ -4,16 +4,18 @@ from models import db, Document
 def process_query(search, _agency, _category, _types):
 	#remove_stop_words(search)
 	#remove unsafe characters
-	unsafe_chars = ['"', '<', '>', '#', '%', '{', '}', '|', '\\', '^', '~', '[', ']', '`', "'"]
+	unsafe_chars = ['"', '<', '>', '#', '%', '{', '}', '|', '\\', '^', '~', '[', ']', '`', "'",  "--"]
 	for char in unsafe_chars:
 		search = search.replace(char, '')
 	print search
 	search = search.split()
 
-	results = Document.query.filter(Document.title.contains(search[0]))
-	for i in range(1, len(search)):
-		results = results.filter(Document.title.contains(search[i]))
-
+	results_t = Document.query
+	results_d = Document.query
+	for i in range(0, len(search)):
+		results_t = results_t.filter(Document.title.contains(search[i]))
+		results_d = results_d.filter(Document.description.contains(search[i]))
+	results = results_t.union(results_d)
 	if _agency != "All Agencies":
 		results = results.filter(Document.agency == _agency)
 	if _category != "All Categories":
